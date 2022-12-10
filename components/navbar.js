ba3959eb807f95d0b26353d530aa531e19d9cfa7
Hakymichi <awesomeabdu@gmail.com>
2022-12-10 04:02:35 +0300

    Added Image and Text

--- components/navbar.js
+++ components/navbar.js
@@ -1,42 +1,45 @@
 import Link from "next/link";
 import styles from "../styles/navbar.module.css";
 import { FiDownload } from "react-icons/fi";
+import React from "react";
 function Navbar(props) {
   return (
-    <nav>
-      <ul className={styles.ul}>
-        <li>
-          <Link className={styles.logopad} href="/#">
-            HakyCodes
-          </Link>
-        </li>
-        <li className={props.active == "/about" ? styles.active : ""}>
-          <Link className={styles.link} href="/about">
-            About
-          </Link>
-        </li>
-        <li className={props.active == "/projects" ? styles.active : ""}>
-          <Link className={styles.link} href="/projects">
-            Projects
-          </Link>
-        </li>
-        <li className={props.active == "/contact" ? styles.active : ""}>
-          <Link className={styles.link} href="/contact">
-            Contact
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={styles.link}
-            href="/files/resume.pdf"
-            target="_blank"
-            download
-          >
-            <FiDownload /> Resume
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <React.Fragment>
+      <nav>
+        <ul className={styles.ul}>
+          <li>
+            <Link className={styles.logopad} href="/#">
+              HakyCodes
+            </Link>
+          </li>
+          <li className={props.active == "/about" ? styles.active : ""}>
+            <Link className={styles.link} href="/about">
+              About
+            </Link>
+          </li>
+          <li className={props.active == "/projects" ? styles.active : ""}>
+            <Link className={styles.link} href="/projects">
+              Projects
+            </Link>
+          </li>
+          <li className={props.active == "/contact" ? styles.active : ""}>
+            <Link className={styles.link} href="/contact">
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={styles.link}
+              href="/files/resume.pdf"
+              target="_blank"
+              download
+            >
+              <FiDownload /> Resume
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </React.Fragment>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,22 @@
-import { Fragment } from "react";
+import styles from "../styles/index.module.css";
+import Link from "next/link";
 export default function Home() {
-  return <Fragment></Fragment>;
+  return (
+    <div className={styles.parent}>
+      <div className={styles.text}>
+        <h2>
+          Well aren't You lucky to find{" "}
+          <span className={styles.name}>Hakymichi?</span>
+        </h2>
+        <p>
+          Yeah, thank me later. <br /> Hi, I'm a Software Engineer, Tech
+          enthusiast and of course an anime watcher, Hakymichi
+        </p>
+        <Link href="/contact"> Ready to serve your needs!</Link>
+      </div>
+      <div className={styles.pic}>
+        <img className={styles.img} src="homepage.jpg" />
+      </div>
+    </div>
+  );
 }
