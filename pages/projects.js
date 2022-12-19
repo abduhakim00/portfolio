12825f4667a66b98609a324c21949ebe23b6abe8
Hakymichi <awesomeabdu@gmail.com>
2022-12-19 04:52:55 +0300

    added projects layout and animation

--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,36 @@
-function Projects(){
-    return (
-        <h1> Contact Me!</h1>
-        )   
+import React, { useState } from "react";
+import styles from "../styles/projects.module.css";
+function Projects() {
+  let [anime, setanime] = useState(false);
+  let projects = [1, 2, 3];
+  const animate = () => {
+    setanime((prev) => {
+      return !prev;
+    });
+  };
+  return (
+    <React.Fragment>
+      <h1 className={styles.h1}>Some of the recent projects</h1>
+      <main className={styles.main}>
+        {projects.map((el) => {
+          return (
+            <div onClick={animate}>
+              <img
+                src="/notfound.png"
+                alt="Project Image"
+                className={`${styles.img} ${anime ? styles.imgShift : ""}`}
+              />
+              <h2 className={`${styles.h2} ${anime ? styles.opac : ""}`}>
+                Title{" "}
+              </h2>
+              <p className={`${styles.p} ${anime ? styles.opac : ""}`}>
+                Description{" "}
+              </p>
+            </div>
+          );
+        })}
+      </main>
+    </React.Fragment>
+  );
 }
-export default Projects
+export default Projects;
